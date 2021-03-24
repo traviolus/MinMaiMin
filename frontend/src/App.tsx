ad9682b9208c519682d6cb422516ea32e444ac7f
Pattradanai Punvichartkul <saint12302@gmail.com>
2021-03-25 00:59:04 +0700

    update gaugemeter and resultbuttons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import SearchPage from './components/SearchPage';
+import ResultCard from './components/ResultCard';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <SearchPage />
+      <ResultCard />
     </div>
   );
 }
--- frontend/src/components/ResultCard.tsx
+++ frontend/src/components/ResultCard.tsx
@@ -1,28 +1,24 @@
 import React from "react";
-import Gauge from "react-svg-gauge";
-
-function getHexColor(value: number) {
-  let string = value.toString(16);
-  return string.length === 1 ? "0" + string : string;
-}
+import GaugeMeter from "./GaugeMeter";
+import ResultButtons from "./ResultButtons";
 
 interface AppProps {}
 
 interface AppState {
   value: number;
 }
-
 export default class ResultCard extends React.Component<AppProps, AppState> {
+
+  
   constructor(props: AppProps) {
     super(props);
-    this.state = {
-      value: 50,
-    };
   }
+
   render() {
     return (
       <div>
-        <Gauge value={this.state.value} width={400} height={320} label="This is my Gauge" />
+        <GaugeMeter />
+        <ResultButtons />
       </div>
     );
   }
