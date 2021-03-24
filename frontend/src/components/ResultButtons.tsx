b6df6920bf34537442950ecd5b3360922f3b71c3
traviolus <david.tumcharoen@gmail.com>
2021-03-25 04:29:24 +0700

    frontend: fix deployment

--- frontend/src/components/ResultButtons.tsx
+++ frontend/src/components/ResultButtons.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import Grid from "@material-ui/core/grid";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
 // const useStyles = makeStyles((theme: Theme) =>
 //   createStyles({
