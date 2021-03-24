9949ac7d45930f4bb836af8b6731e1edc9c4b1ff
Pattradanai Punvichartkul <saint12302@gmail.com>
2021-03-25 06:21:10 +0700

    add dialogs

--- frontend/src/components/ResultButtons.tsx
+++ frontend/src/components/ResultButtons.tsx
@@ -1,20 +1,81 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import Grid from "@material-ui/core/grid";
+import Dialog from "@material-ui/core/Dialog";
+import {
+  createStyles,
+  Theme,
+  withStyles,
+  WithStyles,
+} from "@material-ui/core/styles";
+import MuiDialogTitle from "@material-ui/core/DialogTitle";
+import MuiDialogContent from "@material-ui/core/DialogContent";
+import MuiDialogActions from "@material-ui/core/DialogActions";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import Typography from "@material-ui/core/Typography";
 
-// const useStyles = makeStyles((theme: Theme) =>
-//   createStyles({
-//     menuButton: {
-//       marginRight: theme.spacing(2),
-//     },
-//     title: {
-//       flexGrow: 1,
-//       marginLeft: "1.75em",
-//     },
-//   })
-// );
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      margin: 0,
+      padding: theme.spacing(2),
+    },
+    closeButton: {
+      position: "absolute",
+      right: theme.spacing(1),
+      top: theme.spacing(1),
+      color: theme.palette.grey[500],
+    },
+  });
+
+export interface DialogTitleProps extends WithStyles<typeof styles> {
+  id: string;
+  children: React.ReactNode;
+  onClose: () => void;
+}
+
+const DialogTitle = withStyles(styles)((props: DialogTitleProps) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme: Theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme: Theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
 
 export default function ResultButtons() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <div>
       <Grid container spacing={3}>
@@ -25,9 +86,33 @@ export default function ResultButtons() {
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button variant="contained" color="primary" size="large">
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={handleClickOpen}
+          >
             ติดต่อทนาย
           </Button>
+          <Dialog
+            onClose={handleClose}
+            aria-labelledby="customized-dialog-title"
+            open={open}
+          >
+            <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+              ประกาศ
+            </DialogTitle>
+            <DialogContent dividers>
+              <Typography gutterBottom>
+              ขณะนี้ ทนายที่กำลังให้บริการเต็มหมดทุกคู่สาย กรุณารอซักครู่
+              </Typography>
+            </DialogContent>
+            <DialogActions>
+              <Button autoFocus onClick={handleClose} color="primary">
+                ปิดหน้าต่าง
+              </Button>
+            </DialogActions>
+          </Dialog>
         </Grid>
         <Grid item xs={3} />
       </Grid>
