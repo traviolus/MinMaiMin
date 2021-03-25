daa2f8ef047c9421852a3fd55bf25c1a5dff23ca
Pattradanai Punvichartkul <saint12302@gmail.com>
2021-03-25 08:22:25 +0700

    add package react-to-print

--- frontend/src/components/ReportCard.tsx
+++ frontend/src/components/ReportCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Paper from "@material-ui/core/Paper";
 import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
 import { Report } from "../model/report.model";
+import Typography from "@material-ui/core/Typography";
 
 function dateFormat(date: Date): string {
   return date.toLocaleDateString("th-TH", {
@@ -28,13 +29,18 @@ function ReportCard(props: Report) {
   return (
     <div className={classes.root}>
       <Paper elevation={3} className={classes.paper}>
-        <p style={{fontWeight: 'bold'}}>รายงานผล</p>
-        <p>ประมวลกฎหมายที่เกี่ยวข้อง: {props.title ?? "หมิ่นประมาท มาตรา 326"}</p>
-				<p>กฎหมายที่ควรพิจารณา: {props.otherLawSections ?? 'ประมวลอาญา ม.393, พ.ร.บ.คอมพิวเตอร์ ม.14'}</p>
+        <p style={{ fontWeight: "bold" }}>รายงานผล</p>
+        <p>
+          ประมวลกฎหมายที่เกี่ยวข้อง: {props.title ?? "หมิ่นประมาท มาตรา 326"}
+        </p>
         <p>
           ดำเนินการฟ้องร้องได้ถึงวันที่<span> </span>
           {dateFormat(props.prescribedDate ?? new Date())}
         </p>
+        <Typography color="textSecondary" gutterBottom style={{fontSize:"small"}}>
+          หมายเหตุ: กฎหมายที่ควรพิจารณา{" "}
+          {props.otherLawSections ?? "ประมวลอาญา ม.393, พ.ร.บ.คอมพิวเตอร์ ม.14"}
+        </Typography>
       </Paper>
     </div>
   );
