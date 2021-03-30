9bf8d080f5e2af9db8df75aec1da1e9b2c5a0a13
Pattradanai Punvichartkul <saint12302@gmail.com>
2021-03-30 19:39:23 +0700

    remove dependency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,20 +3,15 @@ import './App.css';
 import NavBar from './components/NavBar';
 import SearchPage from './components/SearchPage';
 import ResultCard from './components/ResultCard';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const homePage = () => <div><NavBar /><SearchPage /></div>;
 
 function App() {
   return (
-    <Router>  
-      <div className="App">
-        <Route path="/" render={ homePage }/>
-        {/* <NavBar />
-        <SearchPage />
-        <ResultCard /> */}
-      </div>
-    </Router>
+    <div className="App">
+      <NavBar />
+      <SearchPage />
+      <ResultCard />
+    </div>
   );
 }
 
