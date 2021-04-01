7e542216b19854118992923d516f8d2adbc0cc7f
traviolus <david.tumcharoen@gmail.com>
2021-04-02 06:28:05 +0700

    backend: fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from routers import router
 
 app = FastAPI(
@@ -7,6 +8,14 @@ app = FastAPI(
     description="Backend system for E.L.C.'s project => MinMaiMin.",
     version="0.1",
     docs_url="/docs/"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
